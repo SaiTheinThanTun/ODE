e82a609452d907b6b3c66c539cd52d31cdc4bdf3
SaiTheinThanTun <theinthantun.sai@gmail.com>
2016-11-14 16:45:24 +0630

    model getting to equilibrium
    model getting to equilibrium
    because the death rates of both infected and susceptible parts are now
    equal. before that, the model couldn't reach equilibrium easiliy

--- ode-runningscripts.R
+++ ode-runningscripts.R
@@ -1,7 +1,8 @@
 library(deSolve)
 library(maemod)
 
-maxtime <- 1000
+#maxtime <- 1000
+maxtime <- 10000
 
 out <- maemodrun("D:\\Dropbox\\IBM project_Sai\\ODE\\SIRSI.txt", timegrid = seq(0,maxtime,1)) #scenario2
 #out <- maemodrun("SIRSI - Copy.txt", timegrid = seq(0,10000,1)) #scenario1
@@ -10,7 +11,7 @@ head(out)
 tail(out)
 
 #humans
-plot(out[,2], type='l', ylim=c(0,max(c(out[,2],out[,4]))), col="blue", main="Humans")
+plot(out[,2]+out[,6], type='l', ylim=c(0,max(c(out[,2],out[,4]))), col="blue", main="Humans")
 lines(out[,4], col="red")
 lines(out[,7]+out[,8], col="purple")
 
@@ -19,7 +20,7 @@ plot(out[,9], type='l', ylim=c(0,max(c(out[,9],out[,10]))), col="blue", main="Mo
 lines(out[,10], col="red")
 
 #lambda
-plot(lambda_H, type='l', ylim=c(0,max(c(lambda_H,lambda_M))), col="blue", main="lambda")
+plot(lambda_H, type='l', ylim=c(0,max(c(lambda_H,lambda_M))), col="blue", main="lambda, blue=humans")
 lines(lambda_M, col="red")
 
 ####################
@@ -41,7 +42,7 @@ plot(inc_mnth, type='l', main = "Incidence per month (not per 1000)")
 plot(prev, type='l', col='blue', main="Prevalence (R_T+I_DA)/N")
 
 #incidence vs prevalence #calculated from inside
-plot(inc,prev, main="incidence vs prevalence")
+plot(prev,inc, main="incidence vs prevalence")
 
 ####################
 ####outside1########
@@ -69,6 +70,21 @@ ci <- out[,11] #cumulative incidence (per day)
 inc2 <- ci[-1] - ci[-length(ci)] #incidence calc from inside 2 (daily)
 #plot(inc2)
 inc2_mnth <- unname(tapply(inc2, (seq_along(inc2)-1) %/% 30, sum)) #incidence calc from inside 2 (monthly)
+
+#prevalence inside 2
+prev2 <- (out[,5] + out[,8]) / (out[,2] + out[,3] +out[,4] +out[,5] +out[,6] +out[,7] +out[,8]) #incidence per timestep
+#prev2 <-  (out[,3]) /(out[,2] + out[,3] +out[,4] +out[,5] +out[,6] +out[,7] +out[,8]) #incidence per timestep
+prev2_mnth <- prev2[seq(1,length(prev2),30)] #incidence per month
+
+#incidence 2 vs prevalence 2
+PrevVsInc2 <- cbind(prev2_mnth,inc2_mnth)
+#PrevVsInc2 <- PrevVsInc2[-(1:9),] #removing the noise from 1st epidemics
+
+PVI2o <- PrevVsInc2[order(PrevVsInc2[,1]),] #ordered version
+plot(PVI2o, type='l')
+
+#prevelance 2 plot
+plot(prev2_mnth, type='l')
 
 #lambda_H
 # (700/320)*3*.3*(200/700)
--- shinyapps/shiny_maemod_v2_20161015/app.R
+++ shinyapps/shiny_maemod_v2_20161015/app.R
@@ -39,11 +39,11 @@ mu_IDA = 1, #same death rate as mu_S
 
 beta = 1/10, #birth rate of mosquitos
 kappa_SM = 1/10, #death rate of mosquitos
-kappa_IM = 1/8, #death rate of infected mosquitos
+kappa_IM = 1/10, #in search of equilibrium :) 1/8, #death rate of infected mosquitos
 
 a = 3, #human feeding rate
-b = .01, #probability of disease transmission per bite for humans
-c = .03 #probability a mosquito becomes infected after biting an infected human
+b = .006, #.01, #probability of disease transmission per bite for humans
+c = .02 #.03 #probability a mosquito becomes infected after biting an infected human
 
 
 !Equations
@@ -53,7 +53,7 @@ m <- M/N #ratio of mosquito to human
 x <- (I_S + I_UA + I_DA)/N #ratio of infected humans
 z <- I_M/M #ratio of infected mosquitos
 lam_M <- a*c*x
-lam_H <- m*a*b*z
+lam_H <- m*a*b*z #FOI for humans
 
 lambda_M <<- c(lambda_M,lam_M)
 lambda_H <<- c(lambda_H,lam_H)
@@ -94,14 +94,14 @@ prev <- c()
 ####comparison#####
 ###################
 #column(4, plotOutput(outputId = "monthly_inc_compare")), #monthly incidence comparison
-column(4, plotOutput(outputId = "monthly_inc")),
+column(4, plotOutput(outputId = "incVsprev")),
 #column(4, plotOutput(outputId = "incidence")), #incidence by timesteps 
-column(4, plotOutput(outputId = "N_inc_daily"))),
+column(4, plotOutput(outputId = "PrevVsInc2_plot"))),
 fluidRow(column(4,""),
-         column(4, plotOutput(outputId = "incVsprev")),
-         column(4, plotOutput(outputId = "N_inc_monthly"))),
+         column(4, plotOutput(outputId = "monthly_inc")),
+         column(4, plotOutput(outputId = "prev2_plot"))),
 fluidRow(column(4,
-                numericInput(inputId = "maxtime", label= "Max time (days)", value=1080),
+                numericInput(inputId = "maxtime", label= "Max time (days)", value=10000), #1080),
                 numericInput(inputId = "rep", label = "Report/Rx rate", value = .70),
                 numericInput(inputId = "gamma", label = "Recovery rate", value = 1/(20))
                 )),
@@ -115,7 +115,7 @@ server <- function(input, output) {
   out <- reactive(maemodrun(input.text=input$model,timegrid=seq(0,input$maxtime,1)))
   output$humans <- renderPlot({
     #humans
-    plot(out()[,2], type='l', ylim=c(0,max(c(out()[,2],out()[,4]))), col="blue", main="Humans")
+    plot(out()[,2]+out()[,6], type='l', ylim=c(0,max(c(out()[,2],out()[,4]))), col="blue", main="Humans")
     lines(out()[,4], col="red")
     lines(out()[,7]+out()[,8], col="purple")
   })
@@ -126,7 +126,7 @@ server <- function(input, output) {
   })
   output$lambda <- renderPlot({
     #lambda
-    plot(lambda_H, type='l', ylim=c(0,max(c(lambda_H,lambda_M))), col="blue", main="lambda")
+    plot(lambda_H, type='l', ylim=c(0,max(c(lambda_H,lambda_M))), col="blue", main="lambda, blue=humans")
     lines(lambda_M, col="red")
   })
   # output$incidence <- renderPlot({
@@ -140,6 +140,11 @@ server <- function(input, output) {
   #inside 2 is calcualted from ci, which is a state variable
   ci <- reactive(out()[,11]) #cumulative incidence (per day)
   inc2 <- reactive({ci()[-1] - ci()[-length(ci())] })#incidence calc from inside 2 (daily)
+  inc2_mnth <- reactive({unname(tapply(inc2(), (seq_along(inc2())-1) %/% 30, sum))}) #incidence calc from inside 2 (monthly)
+  
+  #prevalence inside 2
+  prev2 <- reactive({(out()[,5] + out()[,8]) / (out()[,2] + out()[,3] +out()[,4] +out()[,5] +out()[,6] +out()[,7] +out()[,8])})  #incidence per timestep
+  prev2_mnth <- reactive(prev2()[seq(1,length(prev2()),30)]) #incidence per month
   
   ###################
   ####comparison#####
@@ -161,32 +166,34 @@ server <- function(input, output) {
   # })
   
   output$monthly_inc <- renderPlot({
-
-    inc2_mnth <- unname(tapply(inc2(), (seq_along(inc2())-1) %/% 30, sum)) #incidence calc from inside 2 (monthly)
-    
-    plot(inc2_mnth, type='l', main="Incidence per month per 1000", col="blue")
+    plot(inc2_mnth(), type='l', main="Incidence per month per 1000", col="blue")
   })
   output$prevalence <- renderPlot({
-    #prevalence (R_T+I_DA)/N #calculated from inside
+    #prevalence (R_T+I_DA)/N #calculated from inside1
     plot(prev, type='l', col='blue', main="Prevalence (R_T+I_DA)/N")
   })
   output$incVsprev <- renderPlot({
-    #incidence vs prevalence #calculated from inside
-    plot(inc,prev, main="incidence vs prevalence")
+    #incidence vs prevalence #calculated from inside1
+    plot(prev2()[-1],inc2(), main="incidence vs prevalence per day", type='l')
+    #plot(prev2_mnth(),inc2_mnth(), main="incidence vs prevalence per mnth", type ='l')
   })
   
   #incidence calculated the normal way
   N_inc_R <- reactive(out()[,4]*input$rep*input$gamma) 
   
-  output$N_inc_daily <- renderPlot({
-    #daily incidence
-    plot(N_inc_R(), type = 'l', main = "daily incidence, outside")
-  })
 
-  output$N_inc_monthly <- renderPlot({
-    inc_mnth <- unname(tapply(N_inc_R(), (seq_along(N_inc_R())-1) %/% 30, sum))
-    #monthly incidence
-    plot(inc_mnth, type = 'l', main = "monthly incidence, outside")
+  output$PrevVsInc2_plot <- renderPlot({
+    #incidence 2 vs prevalence 2
+    PrevVsInc2 <- cbind(prev2_mnth(),inc2_mnth())
+    #PrevVsInc2 <- PrevVsInc2[-(1:9),] #removing the noise from 1st epidemics
+    
+    PVI2o <- PrevVsInc2[order(PrevVsInc2[,1]),] #ordered version
+    plot(PVI2o, type='l')
+  })
+  
+  #prevelance 2 plot
+  output$prev2_plot <- renderPlot({
+    plot(prev2_mnth(), type='l')
   })
   
   
