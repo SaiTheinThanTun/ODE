9a2abbceb87dd53c8714a4f468cf3ae39fc684f1
SaiTheinThanTun <theinthantun.sai@gmail.com>
2016-12-01 10:52:19 +0700

    base funtion to reproduce Okiro

--- reproducing Okiro/Okiro.R
+++ reproducing Okiro/Okiro.R
@@ -6,21 +6,27 @@
 library(deSolve)
 rm(list = ls())
 
+#storage
+prevV <- NA
+incV <- NA
+
+no.sim <- 10
+
 ####parameters####
 #parameters in the global environment
 init.pop <- c(S=1000, E=0, I_S=1, R_T=0, S_I=0, I_UA=0, I_DA=0, D = 1000) 
 maxtime <- 10000
+
+####parameters to vary####
 mu_i = 1/(55*364) #birth rate of human
 mu_S = 1/(55*364) #normal death rate of human
-
-pmu <- rate2prob(mu_i) #birth/death probability of human, RUNS FASTER if defined here
-
 omega_0 = 1/(5*364) #total loss of immunity (5 years), #then the model will need to run over 5 years
 mu_E = 1 #same death rate as susceptibles
-f = 1/10 #duration of E is approximately 10 days, Collins
+
+f = c(1/15,1/5) #5 to 10 days #1/10 #duration of E is approximately 10 days, Collins
 mu_IS = 3 # increased death rate due to untreated malaria, 3 times higher chance
-gamma = 1/(20) #recovery rate for both natural recovery and treatment
-rep = .70 # treatment+report coverage
+gamma = c(1/30,1/15) #1/(20) #recovery rate for both natural recovery and treatment
+rep = .7 #c(.2,.8) # .70 # treatment+report coverage @@@@@ this can't be varied since its effect is very significant on the incidence and prevalence
 mu_RT = 1.0001 # increased death rate from treatment, 1.0001 times
 sigma = 1/(6*30) # loss of prophylactic effect of drugs/treatment
 mu_SI = 1 # same death rate as mu_S
@@ -40,49 +46,16 @@ beta_ = 1/10 #birth rate of mosquitos
 kappa_SM = 1/10 #death rate of mosquitos
 kappa_IM = 1/10 #in search of equilibrium :) 1/8, #death rate of infected mosquitos
 
-a = 3 #human feeding rate
-b = .006 #.01, #probability of disease transmission per bite for humans
-c_ = .02 #.03 #probability a mosquito becomes infected after biting an infected human
+a = c(1,3) #human feeding rate
+b = c(.006,.01)#.006 #.01, #probability of disease transmission per bite for humans
+c_ = c(.02,.08) #.02 #.03 #probability a mosquito becomes infected after biting an infected human
 #seasonality
-seas_switch <- 1 #logical switch for seasonality
+seas_switch <- 0 #logical switch for seasonality
 amp <- .2 #.7 or 1 Lisa's advice
 phi <- 0
 magnitude <- 1
 
 ####ODE everything under here####
-#parameterizing for ODE
-times <- seq(0, maxtime, by = 1)
-mosquitostates <- c(S_M=S_M,I_M=I_M, ci=0) #has to be EXACTLY like this!!!!
-param_ode <- c(
-  mu_i = mu_i,
-  mu_S = mu_S,
-  omega_0 = omega_0,
-  mu_E = mu_E,
-  f = f,
-  mu_IS = mu_IS,
-  gamma = gamma,
-  rep = rep,
-  mu_RT = mu_RT,
-  sigma = sigma,
-  mu_SI = mu_SI,
-  xi1 = xi1,
-  xi2 = xi2,
-  omega_IUA = omega_IUA,
-  omega_IDA = omega_IDA,
-  mu_IUA = mu_IUA,
-  mu_IDA = mu_IDA,
-  beta_ = beta_,
-  kappa_SM = kappa_SM,
-  kappa_IM = kappa_IM,
-  a = a,
-  b = b,
-  c_ = c_,
-  seas_switch = seas_switch,
-  amp = amp,
-  phi = phi,
-  magnitude = magnitude
-)
-
 
 #transient values in ODE
 transient_ode <- c("N <- S + E + I_S + R_T + S_I + I_UA + I_DA",
@@ -125,26 +98,60 @@ testfun <- function(t, state, parameters)
        })
 }
 
-out <-  ode(y = c(init.pop[-length(init.pop)], mosquitostates), times = times, func = testfun,parms = param_ode)
+for(i in 1:no.sim){
+  #parameterizing for ODE
+  times <- seq(0, maxtime, by = 1)
+  mosquitostates <- c(S_M=S_M,I_M=I_M, ci=0) #has to be EXACTLY like this!!!!
+  param_ode <- c(
+    mu_i = mu_i,
+    mu_S = mu_S,
+    omega_0 = omega_0,
+    mu_E = mu_E,
+    f = runif(1, f[1],f[2]),
+    mu_IS = mu_IS,
+    gamma = runif(1, gamma[1],gamma[2]),
+    rep = rep,
+    mu_RT = mu_RT,
+    sigma = sigma,
+    mu_SI = mu_SI,
+    xi1 = xi1,
+    xi2 = xi2,
+    omega_IUA = omega_IUA,
+    omega_IDA = omega_IDA,
+    mu_IUA = mu_IUA,
+    mu_IDA = mu_IDA,
+    beta_ = beta_,
+    kappa_SM = kappa_SM,
+    kappa_IM = kappa_IM,
+    a = runif(1,a[1],a[2]),
+    b = runif(1,b[1],b[2]),
+    c_ = runif(1,c_[1],c_[2]),
+    seas_switch = seas_switch,
+    amp = amp,
+    phi = phi,
+    magnitude = magnitude
+  )
+  out <-  ode(y = c(init.pop[-length(init.pop)], mosquitostates), times = times, func = testfun,parms = param_ode)
+  
+  #inside 2 is calcualted from ci, which is a state variable
+  ci <- out[,11] #cumulative incidence (per day)
+  inc2 <- ci[-1] - ci[-length(ci)] #incidence calc from inside 2 (daily)
+  inc2_mnth <- unname(tapply(inc2, (seq_along(inc2)-1) %/% 30, sum)) #incidence calc from inside 2 (monthly)
+  
+  #prevalence inside 2
+  prev2 <- (out[,5] + out[,8]) / (out[,2] + out[,3] +out[,4] +out[,5] +out[,6] +out[,7] +out[,8])  #incidence per timestep
+  prev2_mnth <- prev2[seq(1,length(prev2),30)] #incidence per month
+  
+  #####plot ODE ALONE####
+  # plot(out[,2]+out[,6], ylim=c(0, sum(init.pop[-length(init.pop)])), type='l', col="blue", main="ODE alone")
+  # lines(out[,4], col="red")
+  # lines(out[,7]+out[,8], col="purple")
+  # legend("top",legend=c("Susceptibles","Infected","Asymptomatic Infected"),
+  #        text.col=c("blue","red","purple"),pch= "__", col=c("blue", "red","purple"))
+  
+  #####plot prev vs incidence####
+  prevV[i] <- tail(prev2_mnth,2)[1]
+  incV[i] <- tail(inc2_mnth,2)[1] ######Can't take last point since it maynot be a full month!!!!!!!!!!!!!!!!###########
+}
 
-
-#inside 2 is calcualted from ci, which is a state variable
-ci <- out[,11] #cumulative incidence (per day)
-inc2 <- ci[-1] - ci[-length(ci)] #incidence calc from inside 2 (daily)
-inc2_mnth <- unname(tapply(inc2, (seq_along(inc2)-1) %/% 30, sum)) #incidence calc from inside 2 (monthly)
-
-#prevalence inside 2
-prev2 <- (out[,5] + out[,8]) / (out[,2] + out[,3] +out[,4] +out[,5] +out[,6] +out[,7] +out[,8])  #incidence per timestep
-prev2_mnth <- prev2[seq(1,length(prev2),30)] #incidence per month
-
-#####plot ODE ALONE####
-plot(out[,2]+out[,6], ylim=c(0, sum(init.pop[-length(init.pop)])), type='l', col="blue", main="ODE alone")
-lines(out[,4], col="red")
-lines(out[,7]+out[,8], col="purple")
-legend("top",legend=c("Susceptibles","Infected","Asymptomatic Infected"),
-       text.col=c("blue","red","purple"),pch= "__", col=c("blue", "red","purple"))
-
-#####plot prev vs incidence####
-plot(prev2_mnth,inc2_mnth, type='l', main="Monthly incidence vs prevalence \n Can't take last point since it maynot be a full month")
-tail(prev2_mnth)
-tail(inc2_mnth) ######Can't take last point since it maynot be a full month!!!!!!!!!!!!!!!!###########
+plot(prevV,incV, main="Monthly incidence vs prevalence \n Using various combination of parameters")
